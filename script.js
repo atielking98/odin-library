2fe107e9627b3e082ae2a9d14022a9637368902f
Allison Tielking <atielkin@alumni.stanford.edu>
2022-11-22 10:05:21 -0600

    book form

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
 let myLibrary = [];
 
-function Book(title, author, pages, haveRead) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.haveRead = haveRead
-  this.info = function() {
-    return title + " by " + author + ", " + pages + 
-        " pages long. Read already? " + (haveRead ? "Yes" : "No"); 
-  }
+class Book {
+constructor(
+    title = 'Unknown',
+    author = 'Unknown',
+    pages = 0,
+    isRead = false
+) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+}
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+class Library {
+    constructor() {
+        this.myBooks = [];
+    }
+    addBook(newBook) {
+        if (!this.isInLibrary(newBook)) {
+            this.myBooks.push(newBook);
+            console.log("Book Added!");
+        }
+    }
+
+    isInLibrary(newBook) {
+        return this.myBooks.some((book) => book.title === newBook.title)
+    }
 }
+
 
 function displayBooks() {
     myLibrary.forEach((book) => {
         return book;
     })
 }
+
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const isRead = document.getElementById('have-read').checked;
+    return new Book(title, author, pages, isRead);
+  }
+  
+const addBook = (e) => {
+    e.preventDefault();
+    const newBook = getBookFromInput();
+    library.addBook(newBook);
+}
+
+const library = new Library();
+
+const addBookForm = document.getElementById('book-form');
+addBookForm.onsubmit = addBook;
